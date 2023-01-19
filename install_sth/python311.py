85a721eb60375b5f6f5695bad7224225ea95064c
mlh <mlh420@qq.com>
2023-01-19 23:00:57 +0800

    fix bzip's err when install python311

--- install_sth/python311.py
+++ install_sth/python311.py
@@ -2,11 +2,12 @@ import os
 import subprocess
 # https://www.workaround.cz/howto-build-compile-install-latest-python-311-310-39-38-37-centos-7-8-9/
 os.system('sudo yum -y update')
-os.system('sudo yum -y install wget yum-utils gcc openssl-devel bzip2-devel libffi-devel')
+os.system('sudo yum -y install wget yum-utils gcc openssl-devel bzip-devel bzip2-devel libffi-devel')
 
 tmp_cwd = '/tmp/'
 download = """wget https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tgz"""
-subprocess.run(download, shell=True, cwd=tmp_cwd)
+if not os.path.exists('/tmp/Python-3.11.1.tgz'):
+    subprocess.run(download, shell=True, cwd=tmp_cwd)
 
 decompression_python = 'tar xzf Python-3.11.1.tgz'
 subprocess.run(decompression_python, shell=True, cwd=tmp_cwd)
