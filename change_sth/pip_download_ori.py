dc6eb9737607a04b843c66e3dcd78f548188895e
mlh <mlh420@qq.com>
2023-02-28 16:07:58 +0800

    换豆瓣源

--- change_sth/pip_download_ori.py
+++ change_sth/pip_download_ori.py
@@ -4,8 +4,9 @@ os.system('yum -y install python3-pip')
 pip_dir = '/root/.pip'
 pip_conf_path = '/root/.pip/pip.conf'
 download_ori_content = """[global]
-    index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+    index-url = http://pypi.douban.com/simple
 """
+# index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 if not os.path.exists(pip_conf_path):
     os.mkdir(pip_dir)
     with open(pip_conf_path, mode='w+')as f:
