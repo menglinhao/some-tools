606f3090f758004fd53f989d15a9d34b7470bfff
mlh <mlh420@qq.com>
2023-03-03 08:23:58 +0800

    zsh: dst theme

--- install_sth/zsh.py
+++ install_sth/zsh.py
@@ -1,5 +1,6 @@
 import os
 import re
+
 # https://blog.csdn.net/MrKorbin/article/details/124777136?spm=1001.2014.3001.5506
 
 
@@ -9,8 +10,10 @@ if not os.path.exists('~/.oh-my-zsh'):
     os.system('yum -y install zsh')
     os.system('git clone https://gitee.com/whereabouts-fork/ohmyzsh.git ~/.oh-my-zsh')
     # plugins
-    os.system('git clone https://gitee.com/whereabouts-fork/zsh-autosuggestions.git ~/.oh-my-zsh//plugins/zsh-autosuggestions')
-    os.system('git clone https://gitee.com/whereabouts-fork/zsh-syntax-highlighting.git ~/.oh-my-zsh//plugins/zsh-syntax-highlighting')
+    os.system(
+        'git clone https://gitee.com/whereabouts-fork/zsh-autosuggestions.git ~/.oh-my-zsh//plugins/zsh-autosuggestions')
+    os.system(
+        'git clone https://gitee.com/whereabouts-fork/zsh-syntax-highlighting.git ~/.oh-my-zsh//plugins/zsh-syntax-highlighting')
     os.system('cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc')
 
 plugins_pattern = r"plugins=\(git\)"
@@ -20,19 +23,24 @@ zsh_path = '/root/.zshrc'
 theme_pattern = r'ZSH_THEME=(.*)?\n'
 # amuse 也不错 需要安装
 # dnf install powerline-fonts
-new_theme = 'ZSH_THEME="bureau"' 
+# new_theme = 'ZSH_THEME="bureau"'
+new_theme = 'ZSH_THEME="dst"'
 
 if os.path.exists(zsh_path):
     with open(zsh_path, mode='r+') as f:
         content = f.read()
 
         new_content = re.sub(plugins_pattern, new_plugins, content)
-        new_content = re.sub(theme_pattern, new_theme,new_content)
+        new_content = re.sub(theme_pattern, new_theme, new_content)
 
         f.write(new_content)
 
 os.system(f'source {zsh_path}')
 print("git ok")
+""" wsl 启用zsh默认
+vim ~/.bashrc
+bash -c zsh
+"""
 
 """
 # vim ~/.zshrc
