debfb1ca60843656e9f7130a448a3414e18cb6d9
mlh <mlh420@qq.com>
2023-03-21 15:44:42 +0800

    https:douban

--- change_sth/pip_download_ori.py
+++ change_sth/pip_download_ori.py
@@ -4,7 +4,8 @@ os.system('yum -y install python3-pip')
 pip_dir = '/root/.pip'
 pip_conf_path = '/root/.pip/pip.conf'
 download_ori_content = """[global]
-    index-url = http://pypi.douban.com/simple
+    index-url = https://pypi.douban.com/simple
+    trusted-host = https://pypi.douban.com/simple
 """
 # index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 if not os.path.exists(pip_conf_path):
