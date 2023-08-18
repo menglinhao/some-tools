e73168b7a48e6c7aca82bbf94615ee231c37b1ea
mlh <mlh@cn>
2023-08-18 17:20:21 +0800

    use https://pypi.tuna.tsinghua.edu.cn/simple

--- change_sth/pip_download_ori.py
+++ change_sth/pip_download_ori.py
@@ -11,8 +11,8 @@ os.system(f'{install_tool} -y install python3-pip')
 pip_dir = '/root/.pip'
 pip_conf_path = '/root/.pip/pip.conf'
 download_ori_content = """[global]
-    index-url = https://pypi.douban.com/simple
-    trusted-host = https://pypi.douban.com/simple
+    index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+    trusted-host = https://pypi.tuna.tsinghua.edu.cn/simple
 """
 # index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 if not os.path.exists(pip_conf_path):
