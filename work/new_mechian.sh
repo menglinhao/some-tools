b2e793f73c1cca9af88b5da512976729c4322f4e
mlh <mlh@cn>
2023-11-14 14:52:30 +0800

    install first

--- work/new_mechian.sh
+++ work/new_mechian.sh
@@ -4,4 +4,6 @@ yum install -y git
 mlh_dir=/root/mlh
 mkdir -p $mlh_dir
 git clone https://gitee.com/Hey_friends/some_tools.git $mlh_dir
-cd $mlh_dir && python3 some_tools/install_sth/zsh.py
+yum install -y python39
+ln -s /usr/bin/python3.9 /usr/bin/python39 
+cd $mlh_dir && python39 some_tools/install_sth/zsh.py
